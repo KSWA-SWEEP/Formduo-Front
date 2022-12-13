4901ccd091f92a143c196880820d92f8e935ff0d
banggeunho <panggeunho@gmail.com>
2022-12-13 22:00:59 +0900

    [SEO] sitemap & robots.txt 자동 생성 모듈 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,13 @@
 sudo docker ps -a -q --filter "name=frontend-msa" | grep -q . && docker stop frontend-msa && docker rm frontend-msa | true
 
 # 기존 이미지 삭제
-sudo docker rmi jongleur6596/frontend-msa
+sudo docker rmi qkdrmsgh73/service-search
 
 # 도커허브 이미지 pull
-sudo docker pull jongleur6596/frontend-msa
+sudo docker pull qkdrmsgh73/service-search
 
 # 도커 run
-sudo docker run -d -p 3000:3000 --name frontend-msa jongleur6596/frontend-msa
+sudo docker run -d -p 3000:3000 --name service-search qkdrmsgh73/service-search
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 sudo docker rmi -f $(docker images -f "dangling=true" -q) || true
